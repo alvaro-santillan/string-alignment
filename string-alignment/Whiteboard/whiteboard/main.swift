f0cd14955125ec68f5fe75eb69cdbe57fad1d2a8
Álvaro Santillan <31286929+alvaro-santillan@users.noreply.github.com>
2021-02-11 15:08:33 -0700

    alignStrings Testing Complete.

--- string-alignment/Whiteboard/whiteboard/main.swift
+++ string-alignment/Whiteboard/whiteboard/main.swift
@@ -20,19 +20,19 @@ func alignStrings(newStartString: [String], newEndString: [String], insertCost: 
     for x in 0...newStartString.count - 1 {
         var tempArray = [Int]()
         for y in 0...newEndString.count - 1 {
-            tempArray.append(9)
+            tempArray.append(Int.max-2)
         }
         costMatrix.append(tempArray)
     }
 
     // For loop to update matrix to correct values.
-    for x in 0...newStartString.count - 1 {
+    for y in 0...newStartString.count - 1 {
 //        print("yy" + String(y))
-        for y in 0...newEndString.count - 1 {
-            var diagonal = 9
-            var topPlusCost = 9
-            var leftPlusCost = 9
-            var diagonalPlusCost = 9
+        for x in 0...newEndString.count - 1 {
+            var diagonal = Int.max-2
+            var topPlusCost = Int.max-2
+            var leftPlusCost = Int.max-2
+            var diagonalPlusCost = Int.max-2
             
             if (y-1 > -1) && (x-1 > -1) {
                 diagonal = costMatrix[y-1][x-1]
@@ -82,9 +82,9 @@ func alignStrings(newStartString: [String], newEndString: [String], insertCost: 
     }
 
     
-    for i in costMatrix {
-        print(i)
-    }
+//    for i in costMatrix {
+//        print(i)
+//    }
 //    print(costMatrix)
     return costMatrix
 }
@@ -93,12 +93,23 @@ var py = [[0, 1, 2, 3, 4], [1, 0, 1, 2, 3], [2, 1, 2, 3, 2], [3, 2, 1, 2, 3], [4
 
 py = [[0, 1, 2, 3, 4, 5], [1, 0, 1, 2, 3, 4], [2, 1, 0, 1, 2, 3], [3, 2, 1, 0, 1, 2], [4, 3, 2, 1, 2, 3], [5, 4, 3, 2, 1, 2]]
 
-//let sw = alignStrings(newStartString: ["-","B","E","A","R"], newEndString: ["-","B","A","R","E"], insertCost: 1, deleteCost: 1, subCost: 2)
-let sw = alignStrings(newStartString: ["-","P","L","A","I","N"], newEndString: ["-","P","L","A","I","N"], insertCost: 1, deleteCost: 1, subCost: 2)
+py = [[0, 1, 2, 3, 4, 5, 6], [1, 0, 1, 2, 3, 4, 5], [2, 1, 0, 1, 2, 3, 4], [3, 2, 1, 0, 1, 2, 3], [4, 3, 2, 1, 2, 3, 4], [5, 4, 3, 2, 3, 4, 3]]
 
-//if py == sw {
-//    print("pass")
-//} else {
-//    print("fail")
-//    print(sw)
-//}
+py = [[0, 1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 5, 4, 5, 6], [2, 1, 2, 3, 4, 5, 6, 7], [3, 2, 3, 2, 3, 4, 5, 6], [4, 3, 2, 3, 4, 5, 6, 7], [5, 4, 3, 4, 3, 4, 5, 6], [6, 5, 4, 5, 4, 5, 6, 7], [7, 6, 5, 6, 5, 6, 7, 8], [8, 7, 6, 7, 6, 7, 8, 7], [9, 8, 7, 8, 7, 8, 7, 8]]
+py = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [3, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [4, 3, 2, 3, 4, 5, 6, 7, 8, 9, 10], [5, 4, 3, 4, 5, 4, 5, 6, 7, 8, 9], [6, 5, 4, 5, 6, 5, 6, 7, 8, 9, 10], [7, 6, 5, 6, 7, 6, 7, 8, 9, 10, 11], [8, 7, 6, 7, 8, 7, 8, 9, 10, 11, 12], [9, 8, 7, 8, 9, 8, 9, 10, 9, 10, 11], [10, 9, 8, 9, 10, 9, 10, 11, 10, 9, 10], [11, 10, 9, 8, 9, 10, 11, 12, 11, 10, 9]]
+py = [[0, 1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 1, 2, 3, 4, 5, 6, 7], [2, 3, 2, 3, 4, 5, 6, 5, 6], [3, 4, 3, 2, 3, 4, 5, 6, 7], [4, 3, 4, 3, 2, 3, 4, 5, 6], [5, 4, 5, 4, 3, 4, 5, 4, 5], [6, 5, 4, 5, 4, 5, 6, 5, 4], [7, 6, 5, 6, 5, 6, 5, 6, 5], [8, 7, 6, 7, 6, 7, 6, 5, 6], [9, 8, 7, 8, 7, 8, 7, 6, 5]]
+//let sw = alignStrings(newStartString: ["-","B","E","A","R"], newEndString: ["-","B","A","R","E"], insertCost: 1, deleteCost: 1, subCost: 2)
+//let sw = alignStrings(newStartString: ["-","P","L","A","I","N"], newEndString: ["-","P","L","A","N","E"], insertCost: 1, deleteCost: 1, subCost: 2)
+//let sw = alignStrings(newStartString: ["-","F","L","O","U","R"], newEndString: ["-","F","L","O","W","E","R"], insertCost: 1, deleteCost: 1, subCost: 2)
+//let sw = alignStrings(newStartString: ["-","A","H","I","G","H","S","T","E","P"], newEndString: ["-","H","G","I","H","A","P","E"], insertCost: 1, deleteCost: 1, subCost: 2)
+
+let sw = alignStrings(newStartString: ["-","A","C","G","T","C","A","T","C","A"], newEndString: ["-","T","A","G","T","G","T","C","A"], insertCost: 1, deleteCost: 1, subCost: 2)
+
+if py == sw {
+    print("pass")
+} else {
+    print("fail")
+    print(sw)
+}
+
+// FIX MAX Number on exponential
